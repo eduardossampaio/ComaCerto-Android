a390f7715a0565b3e994e552133e43b6bd46dc1c
Eduardo Soares <eduardo.ssampaio@outlook.com>
2018-08-28 16:29:39 -0300

    creating project

---
+++ app/src/main/java/apps/esampaio/com/comacerto/SplashActivity.kt
@@ -0,0 +1,12 @@
+package apps.esampaio.com.comacerto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
