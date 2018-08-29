2e302e4b02c95630e60ff8914ca3d67670c98b0a
Eduardo Soares <eduardo.ssampaio@outlook.com>
2018-08-28 23:16:10 -0300

    feat: Adding new Meal
    Creating initial views for Activity

--- app/src/main/java/apps/esampaio/com/comacerto/view/HomeActivity.kt
+++ app/src/main/java/apps/esampaio/com/comacerto/view/HomeActivity.kt
@@ -1,13 +1,17 @@
 
 package apps.esampaio.com.comacerto.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import apps.esampaio.com.comacerto.R
 import apps.esampaio.com.comacerto.view.help.HelpFragment
-import apps.esampaio.com.comacerto.view.meals.ListMealsFragment
+import apps.esampaio.com.comacerto.view.meals.list.ListMealsFragment
+import apps.esampaio.com.comacerto.view.meals.register.AddNewMealActivity
 import apps.esampaio.com.comacerto.view.report.GenerateReportFragment
 import apps.esampaio.com.comacerto.view.settings.SettingsFragment
 import kotlinx.android.synthetic.main.activity_home.*
@@ -23,24 +27,38 @@ class HomeActivity : AppCompatActivity() {
             when (item.getItemId()) {
                 R.id.navigation_meal_list -> {
                     displayMealFragment()
-                    true
                 }
                 R.id.navigation_meal_reports -> {
                     displayGenerateReportsFragment()
-                    true
                 }
                 R.id.navigation_meal_settings -> {
                     displaySettingsFragment()
-                    true
                 }
                 R.id.navigation_meal_help -> {
                     displayHelpFragment()
-                    true
+
                 }
             }
             true
         }
         displayMealFragment()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val isNewMeal = true
+        if (isNewMeal) {
+            menuInflater.inflate(R.menu.meals_main_menu,menu)
+            return true
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.add_new_meal_menu_item){
+            val intent = Intent(this,AddNewMealActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun displayMealFragment() {
---
+++ app/src/main/java/apps/esampaio/com/comacerto/view/meals/register/AddNewMealActivity.kt
@@ -0,0 +1,13 @@
+package apps.esampaio.com.comacerto.view.meals.register
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import apps.esampaio.com.comacerto.R
+
+class AddNewMealActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_meal)
+    }
+}
